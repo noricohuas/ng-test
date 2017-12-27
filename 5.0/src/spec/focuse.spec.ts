5988a12b8bc396bdec2569ab5514b3fae8d20a5e
huayu <noricohuas@126.com>
2017-12-27 14:17:07 +0800

    focused specs

---
+++ 5.0/src/spec/focuse.spec.ts
@@ -0,0 +1,34 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+
+describe('Focused specs', () => {
+
+    // fit('is focused and will run', () => {
+    //     expect(true).toBeTruthy();
+    // });
+
+    // it('is not focused and will not run', () => {
+    //     expect(true).toBeFalsy();
+    // });
+
+    // fdescribe('focused describe1', () => {
+    //     it('will run', () => {
+    //         expect(true).toBeTruthy();
+    //     });
+
+    //     it('will also run', () => {
+    //         expect(true).toBeTruthy();
+    //     });
+    // });
+
+    // fdescribe('another focused describe', () => {
+    //     fit('is focused and will run', () => {
+    //         expect(true).toBeTruthy();
+    //     });
+
+    //     it('is not focused and will not run', () => {
+    //         expect(true).toBeTruthy();
+    //     });
+    // });
+
+});
